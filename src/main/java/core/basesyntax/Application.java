177aa3562d276845455b19f6e6fd119202cf468f
skviropitek <skvyra.anton@gmail.com>
2026-03-18 14:26:14 +0100

    aaa

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class Application {
 
-    private static final int NUMBER_OF_BALLS = 3;
+    private static final int NUMBER_OF_BALLS = 4;
 
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
